8090cad95b6dd4ebe8939ab7fb85e097184c79bc
ThomasTwomey <league@WTS15.local>
2017-09-24 17:27:08 -0700

    Text Undo Redo

--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -6,10 +6,12 @@ import java.util.Stack;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class TextUndoRedo implements KeyListener {
-	Stack<String> del = new Stack<String>();
+	Stack<Character> del = new Stack<Character>();
+
 	public TextUndoRedo() {
 
 		// TODO Auto-generated constructor stub
@@ -25,7 +27,7 @@ public class TextUndoRedo implements KeyListener {
 		frame.add(panel);
 		panel.add(label);
 		frame.setVisible(true);
-		frame.setSize(200, 100);
+		frame.setSize(500, 50);
 		frame.addKeyListener(this);
 	}
 
@@ -33,21 +35,6 @@ public class TextUndoRedo implements KeyListener {
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
 
-	/*
-	 * Create a JFrame with a JPanel and a JLabel.
-	 * 
-	 * Every time a key is pressed, add that character to the JLabel. It should
-	 * look like a basic text editor.
-	 * 
-	 * Make it so that every time the BACKSPACE key is pressed, the last
-	 * character is erased from the JLabel. Save that deleted character onto a
-	 * Stack of Characters.
-	 * 
-	 * Choose a key to be the Undo key. Make it so that when that key is
-	 * pressed, the top Character is popped off the Stack and added back to the
-	 * JLabel.
-	 * 
-	 */
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -57,7 +44,30 @@ public class TextUndoRedo implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		label.setText(e.getKeyChar());
+		if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+			if(label.getText().length()==0){
+				JOptionPane.showMessageDialog(null, "There is nothing to delete");
+			}
+			String x = label.getText();
+			char c = x.charAt(x.length()-1);
+			String y = x.substring(0, x.length() - 1);
+			label.setText(y);
+			del.push(c);
+		} else if (e.getKeyCode() == KeyEvent.VK_DELETE) {
+			if(del.isEmpty()){
+				JOptionPane.showMessageDialog(null, "There is nothing to undo");
+			}
+			char pop = del.pop();
+			label.setText(label.getText() + pop);
+		} else {
+
+			label.setText(label.getText() + e.getKeyChar());
+		}
+	}
+
+	{
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
